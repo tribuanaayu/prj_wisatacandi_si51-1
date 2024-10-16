68a74b3e474a27c10e1e88dc209d753308d6899e
hermawanumdp <126551695+hermawanumdp@users.noreply.github.com>
2024-10-16 18:32:52 +0700

    Initialize

---
+++ android/app/src/main/kotlin/com/example/prj_wisatacandi_si51/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.prj_wisatacandi_si51
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/screens/detail_screen.dart
@@ -0,0 +1,36 @@
+import 'dart:collection';
+
+import 'package:flutter/material.dart';
+import 'package:prj_wisatacandi_si51/models/candi.dart';
+
+class DetailScreen extends StatelessWidget {
+  final Candi candi;
+
+  const DetailScreen({super.key, required this.candi});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.asset(
+                    candi.imageAsset,
+                    width: double.infinity,
+                    height: 300,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
